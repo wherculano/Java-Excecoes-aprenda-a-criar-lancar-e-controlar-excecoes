c3277bf090dc26bb874c67523d14451b4d6b98a5
wagner <wagner.santos@accurate.com.br>
2023-03-24 17:02:07 -0300

    Utilizacao do try/catch

--- aula_001_pilha_de_execucao/src/Fluxo.java
+++ aula_001_pilha_de_execucao/src/Fluxo.java
@@ -8,14 +8,21 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Inicio do metodo1");
-        metodo2();
+        try {
+            metodo2();
+        } catch (ArithmeticException ex) {
+            System.out.println("ArithmeticException: Não foi possível fazer divisão por 0");
+            // O try/catch estando aqui, na primeira divisao por zero o metodo 2 (loop for) será encerrado.
+        }
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Inicio do metodo2");
-        for(int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
+            int divisaoInteira = i / 0;
+            // Se o try/catch estivesse aqui, o loop todo seria efetuado
         }
         System.out.println("Fim do metodo2");
     }
