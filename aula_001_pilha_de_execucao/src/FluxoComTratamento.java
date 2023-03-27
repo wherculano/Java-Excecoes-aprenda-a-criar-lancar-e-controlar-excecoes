5b5e9d7e47c2788f9c22657f33f884ca6fc6deef
wagner <wagner.santos@accurate.com.br>
2023-03-27 14:43:23 -0300

    Explicacao de Checked e Unchecked - Extends Exception ou RuntimeException

--- aula_001_pilha_de_execucao/src/FluxoComTratamento.java
+++ aula_001_pilha_de_execucao/src/FluxoComTratamento.java
@@ -2,22 +2,24 @@ public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Inicio do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
-
-    private static void metodo1() {
-        System.out.println("Inicio do metodo1");
-        try {
-            metodo2();
-        } catch (ArithmeticException | NullPointerException ex) {
+        try{
+            metodo1();
+        } catch (ArithmeticException | NullPointerException | MyException ex) {
             System.out.println("Exception: " + ex.getMessage());
             // ex.printStackTrace();  // "rastros" do erro
         }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() throws MyException{
+        System.out.println("Inicio do metodo1");
+
+        metodo2();
+
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MyException{
         System.out.println("Inicio do metodo2");
         throw new MyException("Algo deu errado");
         // System.out.println("Fim do metodo2");
--- aula_001_pilha_de_execucao/src/MyException.java
+++ aula_001_pilha_de_execucao/src/MyException.java
@@ -1,5 +1,16 @@
-public class MyException extends RuntimeException{
+public class MyException extends Exception{
     public MyException(String msg){
         super(msg);
     }
 }
+
+/**
+ * Chedked: Quando a classe extende diretamente Exception, ela é 'verificada' pelo compilador.
+   Com isso, obrigatoriamente o metodo que irá usar o
+   throw, deverá usar 'throws MyException' em sua assinatura. Ex.: public void metodo() throws MyException.
+   Ou, o erro deverá ser tratado dentro de um try/catch.
+
+ * Unchecked: Quando a classe extende RuntimeException, não será verificada pelo compilador,
+   assim, não será necessário usar o 'throws', somente o 'throw' dentro de seu corpo/escopo da funcao.
+ **/
+
