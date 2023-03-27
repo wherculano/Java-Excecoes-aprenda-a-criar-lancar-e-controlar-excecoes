49de4a1ae07202be86ea698c8d99b936b46a611a
wagner <wagner.santos@accurate.com.br>
2023-03-27 14:27:06 -0300

    Teste erro recursao: stackoverflow

---
+++ aula_001_pilha_de_execucao/src/FluxoErroJVM.java
@@ -0,0 +1,25 @@
+public class FluxoErroJVM {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio do metodo1");
+        try {
+            metodo2();
+        } catch (ArithmeticException | NullPointerException ex) {
+            System.out.println("Exception: " + ex.getMessage());
+            // ex.printStackTrace();  // "rastros" do erro
+        }
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio do metodo2");
+        metodo2();
+        System.out.println("Fim do metodo2");
+    }
+}
